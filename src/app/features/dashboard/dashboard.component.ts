ff1cf543a2f0f3eca2a4be845dc34afafc33c370
Carlos Ferrer <cafema01@floridauniversitaria.es>
2025-07-19 17:48:55 +0200

    Hacemos el dashboard standAlone

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -5,6 +5,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   imports: [NavbarComponent, RouterOutlet],
+  standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
