904acb9cbf39e99c2f057dd2ad590ec4ef3e7124
Carlos Ferrer <cafema01@floridauniversitaria.es>
2025-07-19 11:46:38 +0200

    Maquetamos el login

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import {
+  FormControl,
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    FormsModule,
+    ButtonModule,
+  ],
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  onSubmit() {}
+}
